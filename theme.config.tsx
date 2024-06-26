1cb4efe8da592c9d38d2764f2f08ed0315e6b54e
Humphrey Liu <humphrey.liu@foxmail.com>
2024-06-26 09:05:02 +0800

    1

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,23 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Card-Number.com</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/humphrey71/documentation-starter-kit',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/humphrey71/documentation-starter-kit/tree/main',
   footer: {
     text: 'Nextra Docs Template',
   },
+  // feedback: {
+  //   content: null,
+  // },
+  // editLink: {
+  //   component:null,
+  // },
 }
 
 export default config
